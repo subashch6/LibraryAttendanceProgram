46eb63afe54fb2982b13bc127588fa2e4b4b9aca
Subash Chebolu <subashch6@gmail.com>
2015-10-27 23:51:55 -0400

    changed the pic of the llama

--- FrontEnd.js
+++ FrontEnd.js
@@ -1,6 +1,5 @@
 function myFunction() 
 {
-	location.href = "https://s-media-cache-ak0.pinimg.com/736x/49/cd/66/49cd6608c4c6bc82d228c28e671972d0.jpg"
-	alert("Be ready For Ze LLAMA");
+	location.href = "https://s-media-cache-ak0.pinimg.com/736x/a1/a9/bb/a1a9bb191b63e26cfbe508c0b241978f.jpg" 
 }
 
